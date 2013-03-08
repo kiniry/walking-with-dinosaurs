82b987c9f2184cd18dc5d155fb4eb694923d8da9
Kasper Reindahl Rasmussen <Reindahl1@hotmail.com>
2013-03-08 14:42:10 +0100

    grammar % error fix

--- bullet-2.81-rev2613/Demos/BasicDemo/Grammar.cpp
+++ bullet-2.81-rev2613/Demos/BasicDemo/Grammar.cpp
@@ -4,7 +4,7 @@ int readDNA(const int* DNA, Physics *world){
 	int index = 0;
 	int blocks = 0;
 	try{	
-		int part = world->createBox(DNA[index]%maxHeight, DNA[index+1]%maxWidth, DNA[index+2]%maxDepth);
+		int part = world->createBox(DNA[index]%maxHeight+1, DNA[index+1]%maxWidth+1, DNA[index+2]%maxDepth+1);
 		blocks++;
 		index=B(index, DNA, world, &blocks, part);
 	}catch (int e){
@@ -64,7 +64,7 @@ int B(int index, const int* DNA, Physics *world, int *blocks, int part){
 }
 
 int J(int index, const int* DNA, Physics *world, int *blocks, int part1){
-	int part2 = world->createBox(DNA[index+10]%maxHeight, DNA[index+11]%maxWidth, DNA[index+12]%maxDepth);
+	int part2 = world->createBox(DNA[index+10]%maxHeight+1, DNA[index+11]%maxWidth+1, DNA[index+12]%maxDepth+1);
 	world->createJoint(part1, part2, DNA[index]%2, DNA[index+1], DNA[index+2], DNA[index+3]%6, DNA[index+4], DNA[index+5], DNA[index+6]%6, DNA[index+7], DNA[index+8], DNA[index+9]);
 	index = B(index+13, DNA, world, blocks, part2);
 	return index;
