c6513dc8a907b9e5ed4e8c57fb02600f0a6229ff
Gustav Lund <Tugsav@gmail.com>
2013-03-13 08:44:49 +0100

    NN Framework Attempt

--- bullet-2.81-rev2613/Demos/BasicDemo/NeuralNode.cpp
+++ bullet-2.81-rev2613/Demos/BasicDemo/NeuralNode.cpp
@@ -1,10 +1,89 @@
 #include "NeuralNode.h"
 
 
-NeuralNode::NeuralNode(void)
+NeuralNode::NeuralNode(float value)
 {
+	this->nrOfInputs=0;
+	this->currentOutput=value;
 }
 
+NeuralNode::NeuralNode(int function, NeuralNode* input, float weight)
+{
+	this->function=function;
+	this->in1=input;
+	this->weight1=weight;
+	this->nrOfInputs=1;
+}
+
+NeuralNode::NeuralNode(int function, NeuralNode* input1, NeuralNode* input2, float weight1, float weight2)
+{
+	this->function=function;
+	this->in1=input1;
+	this->in2=input2;
+	this->weight1=weight1;
+	this->weight2=weight2;
+	this->nrOfInputs=2;
+}
+
+NeuralNode::NeuralNode(int function, NeuralNode* input1, NeuralNode* input2, NeuralNode* input3, float weight1, float weight2, float weight3)
+{
+	this->function=function;
+	this->in1=input1;
+	this->in2=input2;
+	this->in3=input3;
+	this->weight1=weight1;
+	this->weight2=weight2;
+	this->weight3=weight3;
+	this->nrOfInputs=3;
+}
+
+float NeuralNode::getOutput()
+{
+	return this->currentOutput;
+}
+
+void NeuralNode::compute()
+{
+	switch(nrOfInputs){
+	case 0://constant... nothing should happen
+		break;
+	case 1:
+		switch(function){
+		default:
+			perror("Undefined neural function");
+			break;
+		}
+		break;
+	case 2:
+		switch(function){
+		case SUM:
+			currentOutput=sum();
+			break;
+		case MULTIPLY:
+			currentOutput=multiply();
+			break;
+		default:
+			perror("Undefined neural function");
+			break;
+		}
+		break;
+	case 3:
+		switch(function){
+		default:
+			perror("Undefined neural function");
+			break;
+		}
+		break;
+	}
+}
+
+float NeuralNode::sum(){
+	return in1->currentOutput+in2->currentOutput;
+}
+
+float NeuralNode::multiply(){
+	return in1->currentOutput*in2->currentOutput;
+}
 
 NeuralNode::~NeuralNode(void)
 {
--- bullet-2.81-rev2613/Demos/BasicDemo/NeuralNode.h
+++ bullet-2.81-rev2613/Demos/BasicDemo/NeuralNode.h
@@ -1,9 +1,30 @@
 #pragma once
+#include <stdio.h>
+
+//2 inputs
+enum{SUM,MULTIPLY};
+
 class NeuralNode
 {
 	int function;
+	int nrOfInputs;
+	float currentOutput;
+	NeuralNode* in1;
+	NeuralNode* in2;
+	NeuralNode* in3;
+	float weight1,weight2,weight3;
+
 public:
-	NeuralNode(void);
+	NeuralNode(float value);
+	NeuralNode(int function, NeuralNode* input, float weight);
+	NeuralNode(int function, NeuralNode* input1, NeuralNode* input2, float weight1, float weight2);
+	NeuralNode(int function, NeuralNode* input1, NeuralNode* input2, NeuralNode* input3, float weight1, float weight2, float weight3);
 	~NeuralNode(void);
+	float getOutput();
+	void compute();
+
+private:
+	inline float sum();
+	inline float multiply();
 };
 
