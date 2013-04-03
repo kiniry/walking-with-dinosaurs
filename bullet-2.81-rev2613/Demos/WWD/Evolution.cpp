97e3f81fbf4fb13031b4256105b3995fecb5495a
Kasper Reindahl Rasmussen <Reindahl1@hotmail.com>
2013-04-03 12:46:09 +0200

    simple evolution

--- bullet-2.81-rev2613/Demos/WWD/Evolution.cpp
+++ bullet-2.81-rev2613/Demos/WWD/Evolution.cpp
@@ -44,15 +44,56 @@ std::vector<int> mutate(const std::vector<int> dna){
 	return newCreature;
 }
 
-std::vector<int> crossOver1(std::vector<int> dna){
+std::vector<int> crossOver1(std::vector<int> dna1, std::vector<int> dna2){
+	std::vector<int> newCreature;
 
-	return dna;
+	int random1 = rand()%dna1.size();
+	for(int i = 0; i<=random1;i++){
+
+		newCreature.push_back(dna1.at(i));
+
+	}
+
+
+	int random2 = rand()%dna2.size();
+	for(int i = random2; i<dna2.size();i++){
+
+		newCreature.push_back(dna2.at(i));
+
+	}
+
+	return newCreature;
 }
 
-std::vector<int> crossOver2(std::vector<int> dna){
+/**
+*	replaces the middel part of dna1 with a part from dna2
+**/
+std::vector<int> crossOver2(std::vector<int> dna1, std::vector<int> dna2){
+	std::vector<int> newCreature;
+
+	int random1 = rand()%dna1.size();
+	for(int i = 0; i<=random1;i++){
+
+		newCreature.push_back(dna1.at(i));
+
+	}
 
 
-	return dna;
+	int random2 = rand()%dna2.size();
+	int length = rand()%(dna2.size()-random2);
+	for(int i = random2; i<=random2+length;i++){
+
+		newCreature.push_back(dna2.at(i));
+
+	}
+
+
+	for(int i = random1+length; i<=dna1.size();i++){
+
+		newCreature.push_back(dna1.at(i));
+
+	}
+	return newCreature;
 }
 
 
